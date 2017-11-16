333337e7f665ff22c1ff6ce9bf62d6fec0033653
lipeiyu <171388515@qq.com>
2017-11-16 15:01:28 +0800

    新增了-产品详情页面-购买数量按钮的增减数字的变化和根据数据库库存设置了增减的上下限

--- shop-shoes/frontEnd/js/detail.js
+++ shop-shoes/frontEnd/js/detail.js
@@ -1,5 +1,31 @@
 $(function() {
     getDetailData();
+
+    //点击加减按钮
+    var count = 0;
+
+
+
+    $('.mui-content').on('tap','.plus',function() {
+        var num = $(this).data('num');
+        count++;
+        if(count > num) {
+            count = num;
+        }
+        $('.mui-content').find('.num').html(count);
+    })
+    $('.mui-content').on('tap','.sub',function() {
+        count--;
+        if(count < 0) {
+            count = 0;
+        }
+        $('.mui-content').find('.num').html(count);
+    })
+    
+
+
+
+
 })
 
 var getDetailData = function () {
