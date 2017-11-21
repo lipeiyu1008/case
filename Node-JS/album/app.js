0ee456ac8d6f761111001b6181f8520a8ba88562
lipeiyu <171388515@qq.com>
2017-11-21 22:04:40 +0800

    运用了第三方包,增加了新建相册和上传图片功能,并实现了新建和上传与数据库的连接和更新

--- Node-JS/album/app.js
+++ Node-JS/album/app.js
@@ -3,6 +3,22 @@ const express = require('express');
 //引入数据库对象
 const mysql = require('mysql');
 
+//引入核心对象
+const path = require('path');
+
+
+
+
+//解析post请求数据
+const bodyParser = require('body-parser');
+
+//文件增强包
+const fse = require('fs-extra');
+//处理post请求体
+
+const formidable = require('formidable')
+
+
 
 const pool = mysql.createPool({
     connectionLimit: 10,
@@ -43,6 +59,8 @@ router.get('/text',(req,res,next) => {
         if(err) return next(err);
         //使用连接查询所有的album_dir所有数据
         connection.query('select * from album_dir',(error, results)=>{
+             //查询完毕以后，释放连接
+             connection.release();
             //处理查询时带来的异常，比如表名错误
             if(error) return next(error);
             res.render('index.html',{
@@ -51,13 +69,127 @@ router.get('/text',(req,res,next) => {
         })
     });
 })
+//显示照片列表
+.get('/showDir',(req,res,next) => {
+    //获取url地址
+    let dirname = req.query.dir;
+    // console.log(dirname);
 
+    pool.getConnection((err, connection)=> {
+        //处理获取连接时的异常，比如停网了
+        if(err) return next(err);
+        //使用连接查询所有的album_dir所有数据
+        connection.query('select * from album_file where dir=?',[dirname],(error, results)=>{
+            //查询完毕以后，释放连接
+            connection.release();
+            //处理查询时带来的异常，比如表名错误
+            if(err) return next(err);
+            // console.log(results);
+            //记录相册名
+            res.render('album.html',{
+                album:results,
+                dir:dirname
+            })
+        })
+    });
+})
+
+//添加目录
+.post('/albumName',(req,res,next) => {
+    let  dirname = req.body.albumName;
+    pool.getConnection((err, connection)=> {
+        //处理获取连接时的异常，比如停网了
+        if(err) return next(err);
+        //使用连接查询所有的album_dir所有数据
+        connection.query('insert into album_dir values (?)',[dirname],(error, results)=>{
+            //查询完毕以后，释放连接
+            connection.release();
+            //处理查询时带来的异常，比如表名错误
+            if(err) return next(err);
+            // console.log(results);
+            //创建目录
+            const dir = `./resource/${dirname}`;
+
+            fse.ensureDir(dir,err => {
+                //重新定向
+                res.redirect('/showDir?dir=' + dirname);
+            })
+
+        })
+    });
+})
+
+//添加照片
+
+.post('/addPic',(req,res,next) => {
+
+    var form = new formidable.IncomingForm();
+
+    let rootPath = path.join(__dirname,'resource');
+    //设置默认上传目录
+    form.uploadDir = rootPath;
+    form.parse(req, function(err, fields, files) {
+        if(err) return next(err);
+        // console.log(fields);
+        // console.log(files);
+        //文件名称
+        let filename = path.parse(files.pic.path).base;
+        // console.log(filename)
+        //拼接新路径
+        let dist = path.join(rootPath,fields.dir,filename);
+        // console.log(dist);
+        fse.move(files.pic.path,dist,(err) => {
+            if(err) return next(err);
+            // console.log('移动成功')
+
+
+            //将数据保存到数据库
+
+            let db_file = `/resource/${fields.dir}/${filename}`;
+            let db_dir = fields.dir;
+
+            pool.getConnection((err, connection)=> {
+                //处理获取连接时的异常，比如停网了
+                if(err) return next(err);
+                //使用连接查询所有的album_dir所有数据
+                connection.query('insert into album_file values (?,?)',[db_file,db_dir],(error, results)=>{
+                    //查询完毕以后，释放连接
+                    connection.release();
+                    //处理查询时带来的异常，比如表名错误
+                    if(err) return next(err);
+                    // console.log(results);
+                    //重新定向
+                    res.redirect('/showDir?dir=' + db_dir);
+        
+                })
+            });
+
+
+
+
+
+        })
+
+
+
+      });
+
+
+
+
+});
 
 
 
 //处理静态资源
 app.use('/public',express.static('./public'));
+//暴露照片文件夹
+app.use('/resource',express.static('./resource'));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
 app.use(router);
 
